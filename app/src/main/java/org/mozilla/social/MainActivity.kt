d750769cec89565e7a746e0c7262d602434c602d
Devota <daabel@mozilla.com>
2023-07-17 11:26:58 -0400

    Removed intent code from composable

--- app/src/main/java/org/mozilla/social/MainActivity.kt
+++ app/src/main/java/org/mozilla/social/MainActivity.kt
@@ -1,5 +1,6 @@
 package org.mozilla.social
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,9 +8,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.mozilla.social.core.designsystem.theme.MozillaSocialTheme
+import org.mozilla.social.feature.auth.AuthViewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: AuthViewModel by viewModel<AuthViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +27,15 @@ class MainActivity : ComponentActivity() {
                 ) {
                     MainActivityScreen()
                 }
+            }
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        if (intent?.data.toString().startsWith(AuthViewModel.AUTH_SCHEME)) {
+            intent?.data?.let {
+                viewModel.onTokenReceived(it.toString())
             }
         }
     }
--- feature/auth/src/main/java/org/mozilla/social/feature/auth/AuthViewModel.kt
+++ feature/auth/src/main/java/org/mozilla/social/feature/auth/AuthViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import okhttp3.HttpUrl
@@ -12,13 +13,15 @@ import org.mozilla.social.common.logging.Log
 import org.mozilla.social.core.datastore.UserPreferencesDatastore
 import java.net.URL
 
-internal class AuthViewModel(
+ class AuthViewModel(
     private val userPreferencesDatastore: UserPreferencesDatastore,
     private val log: Log,
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow<UiState>(UiState.Default)
     val uiState: StateFlow<UiState> = _uiState
+
+     val isSignedIn = userPreferencesDatastore.dataStore.data.map { it.accessToken != null }
 
     fun onTokenReceived(authUri: String) {
         val httpUrl: HttpUrl? = URL(
