9084a6a02964776c639001df0deb0d984448b331
Devota <daabel@mozilla.com>
2023-07-18 13:42:57 -0400

    Made mastodon service wrapper internal

--- core/data/src/main/java/org/mozilla/social/core/data/repository/repository/FeedRepository.kt
+++ core/data/src/main/java/org/mozilla/social/core/data/repository/repository/FeedRepository.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.map
 import org.mozilla.social.model.Page
 import org.mozilla.social.model.Status
 
-class FeedRepository(private val serviceWrapper: MastodonServiceWrapper) {
+class FeedRepository internal constructor(private val serviceWrapper: MastodonServiceWrapper) {
     fun getPublicTimeline(): Flow<Page<List<Status>>?> =
         serviceWrapper.service.map { it?.getPublicTimeline() }
 }
--- core/data/src/main/java/org/mozilla/social/core/data/repository/repository/MastodonServiceWrapper.kt
+++ core/data/src/main/java/org/mozilla/social/core/data/repository/repository/MastodonServiceWrapper.kt
@@ -11,7 +11,7 @@ import social.mozilla.core.network.MastodonService
 /**
  * This class serves a MastodonService in a flow, once it's initialized with a valid access token
  */
-class MastodonServiceWrapper(userPreferencesDatastore: UserPreferencesDatastore) {
+internal class MastodonServiceWrapper(userPreferencesDatastore: UserPreferencesDatastore) {
 
     val service: StateFlow<MastodonService?> = userPreferencesDatastore.dataStore.data.mapLatest {
         if (it.accessToken != null) {
