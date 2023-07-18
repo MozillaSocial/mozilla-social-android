3e837f8d1c174d404955748da6651e59f836f7d0
Tim <111293209+timc-mozilla@users.noreply.github.com>
2023-07-18 16:06:27 -0500

    init settings work (#34)
    Co-authored-by: JohnOberhauser <14130581+JohnOberhauser@users.noreply.github.com>
    Co-authored-by: John Oberhauser <j.git-global@obez.io>
    Co-authored-by: Devota <daabel@mozilla.com>

---
+++ feature/settings/src/main/java/org/mozilla/social/feature/settings/SettingsScreen.kt
@@ -0,0 +1,258 @@
+package org.mozilla.social.feature.settings
+
+import android.content.Intent
+import android.net.Uri
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.koin.androidx.compose.koinViewModel
+import org.koin.core.parameter.parametersOf
+import org.mozilla.social.core.designsystem.theme.MozillaSocialTheme
+
+@Composable
+fun SettingsRoute(
+    onLogout: () -> Unit,
+    viewModel: SettingsViewModel = koinViewModel(parameters = { parametersOf(onLogout) })
+) {
+    val isToggled = viewModel.isToggled.collectAsState()
+
+    SettingsScreen(
+        isToggled = isToggled.value,
+        viewModel::toggleSwitch,
+        viewModel::logoutUser
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun SettingsScreen(
+    isToggled: Boolean,
+    onSwitchToggled: () -> Unit,
+    logUserOut: () -> Unit
+) {
+    val localContext = LocalContext.current
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.profile_title),
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                },
+            )
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .padding(it)
+                .padding(16.dp)
+        ) {
+            SettingsGroup(name = R.string.group_1) {
+                SettingsSwitch(
+                    name = R.string.switch_text,
+                    icon = R.drawable.ic_placeholder_android,
+                    iconDesc = R.string.ic_switch_explanation,
+                    state = isToggled,
+                    onClick = onSwitchToggled
+                )
+            }
+            SettingsGroup(name = R.string.group_2) {
+                SettingsTextLink(
+                    icon = R.drawable.ic_placeholder_android,
+                    iconDesc = R.string.ic_switch_explanation,
+                    name = R.string.switch_text
+                ) {
+                    val intent = Intent(Intent.ACTION_VIEW).apply {
+                        data = Uri.parse("https://mozilla.social/explore")
+                    }
+                    localContext.startActivity(intent)
+                }
+            }
+            SettingsGroup(name = R.string.logout) {
+                LogoutText(name = R.string.logout) {
+                    logUserOut()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SettingsGroup(
+    @StringRes name: Int,
+    content: @Composable ColumnScope.() -> Unit ){
+    Column(modifier = Modifier.padding(vertical = 8.dp)) {
+        Text(stringResource(id = name))
+        Spacer(modifier = Modifier.height(8.dp))
+        Surface(
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(8),
+        ) {
+            Column {
+                content()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SettingsSwitch(
+    @DrawableRes icon: Int,
+    @StringRes iconDesc: Int,
+    @StringRes name: Int,
+    state: Boolean,
+    onClick: () -> Unit
+) {
+    Surface(
+        color = Color.Transparent,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        onClick = onClick,
+    ) {
+        Column {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painterResource(id = icon),
+                        contentDescription = stringResource(id = iconDesc),
+                        modifier = Modifier.size(24.dp)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = stringResource(id = name),
+                        modifier = Modifier.padding(16.dp),
+                        style = MaterialTheme.typography.bodyMedium,
+                        textAlign = TextAlign.Start,
+                    )
+                }
+                Spacer(modifier = Modifier.weight(1f))
+                Switch(
+                    checked = state,
+                    onCheckedChange = { onClick() }
+                )
+            }
+            Divider()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun SettingsTextLink(
+    @DrawableRes icon: Int,
+    @StringRes iconDesc: Int,
+    @StringRes name: Int,
+    onClick: () -> Unit
+) {
+
+    Surface(
+        color = Color.Transparent,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        onClick = onClick,
+    ) {
+        Column {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painterResource(id = icon),
+                        contentDescription = stringResource(id = iconDesc),
+                        modifier = Modifier.size(24.dp)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(
+                        text = stringResource(id = name),
+                        modifier = Modifier.padding(16.dp),
+                        style = MaterialTheme.typography.bodyMedium,
+                        textAlign = TextAlign.Start,
+                    )
+                }
+                Spacer(modifier = Modifier.weight(1f))
+            }
+            Divider()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun LogoutText(
+    @StringRes name: Int,
+    onClick: () -> Unit
+) {
+
+    Surface(
+        color = MaterialTheme.colorScheme.error,
+        modifier = Modifier
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(8.dp),
+        onClick = onClick,
+    ) {
+        Column {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        text = stringResource(id = name),
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.padding(start = 4.dp, top = 8.dp, bottom = 8.dp),
+                        textAlign = TextAlign.Center,
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+internal fun SettingsScreenPreview() {
+    MozillaSocialTheme {
+        SettingsScreen(false, {}, {})
+    }
+}
