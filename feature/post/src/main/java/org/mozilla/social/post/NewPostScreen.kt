4192280d4781f41247c7b2633b59892b8f8218b8
John Oberhauser <j.git-global@obez.io>
2023-07-25 12:13:12 -0500

    removing scaffold

--- feature/post/src/main/java/org/mozilla/social/post/NewPostScreen.kt
+++ feature/post/src/main/java/org/mozilla/social/post/NewPostScreen.kt
@@ -12,8 +12,8 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -27,21 +27,19 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Send
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -106,14 +104,24 @@ private fun NewPostScreen(
     ) { uri ->
         uri?.let { imageInteractions.onImageInserted(it, it.toFile(context)) }
     }
-
-    Scaffold(
-        topBar = { TopBar(
+    Column {
+        TopBar(
             onPostClicked = onPostClicked,
             onCloseClicked = onCloseClicked,
             sendButtonEnabled = sendButtonEnabled,
-        ) },
-        bottomBar = { BottomBar(
+        )
+        Column(
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            MainBox(
+                statusText = statusText,
+                onStatusTextChanged = onStatusTextChanged,
+                imageStates = imageStates,
+                imageInteractions = imageInteractions,
+            )
+        }
+        BottomBar(
             onUploadImageClicked = {
                 val mediaRequest = PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageAndVideo)
                 if (NewPostViewModel.MAX_IMAGES - imageStates.size <= 1) {
@@ -123,14 +131,6 @@ private fun NewPostScreen(
                 }
             },
             addImageButtonEnabled = addImageButtonEnabled,
-        ) },
-    ) {
-        MainBox(
-            paddingValues = it,
-            statusText = statusText,
-            onStatusTextChanged = onStatusTextChanged,
-            imageStates = imageStates,
-            imageInteractions = imageInteractions,
         )
     }
 }
@@ -142,24 +142,22 @@ private fun TopBar(
     sendButtonEnabled: Boolean,
 ) {
     Column {
-        TopAppBar(
-            title = {},
-            navigationIcon = {
-                IconButton(
-                    onClick = { onCloseClicked() },
-                ) {
-                    Icon(Icons.Default.Close, "close")
-                }
-            },
-            actions = {
-                IconButton(
-                    enabled = sendButtonEnabled,
-                    onClick = { onPostClicked() },
-                ) {
-                    Icon(Icons.Default.Send, "post")
-                }
+        Box(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            IconButton(
+                onClick = { onCloseClicked() },
+            ) {
+                Icon(Icons.Default.Close, "close")
             }
-        )
+            IconButton(
+                modifier = Modifier.align(Alignment.CenterEnd),
+                enabled = sendButtonEnabled,
+                onClick = { onPostClicked() },
+            ) {
+                Icon(Icons.Default.Send, "post")
+            }
+        }
         Divider(
             color = MaterialTheme.colorScheme.outlineVariant
         )
@@ -175,7 +173,7 @@ private fun BottomBar(
         Divider(
             color = MaterialTheme.colorScheme.outlineVariant
         )
-        BottomAppBar(
+        Row(
             modifier = Modifier.height(60.dp)
         ) {
             IconButton(
@@ -191,7 +189,6 @@ private fun BottomBar(
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 private fun MainBox(
-    paddingValues: PaddingValues,
     statusText: String,
     onStatusTextChanged: (String) -> Unit,
     imageStates: Map<Uri, ImageState>,
@@ -206,7 +203,6 @@ private fun MainBox(
         val textFieldFocusRequester = remember { FocusRequester() }
         Surface(
             modifier = Modifier
-                .padding(paddingValues)
                 .fillMaxSize()
                 .clickable {
                     textFieldFocusRequester.requestFocus()
