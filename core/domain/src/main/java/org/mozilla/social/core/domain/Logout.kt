229af78323ff1ee521b464ba22272011e8e3508d
John Oberhauser <j.git-global@obez.io>
2023-09-12 11:32:38 -0500

    clearing social database on logout

--- core/domain/src/main/java/org/mozilla/social/core/domain/Logout.kt
+++ core/domain/src/main/java/org/mozilla/social/core/domain/Logout.kt
@@ -1,12 +1,16 @@
 package org.mozilla.social.core.domain
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.mozilla.social.core.database.SocialDatabase
 import org.mozilla.social.core.datastore.UserPreferencesDatastore
 
 class Logout(
     private val userPreferencesDatastore: UserPreferencesDatastore,
+    private val socialDatabase: SocialDatabase,
 ) {
-    suspend operator fun invoke() {
+    suspend operator fun invoke() = withContext(Dispatchers.IO) {
         userPreferencesDatastore.clearData()
-        // TODO clear out DB and any other data
+        socialDatabase.clearAllTables()
     }
 }
