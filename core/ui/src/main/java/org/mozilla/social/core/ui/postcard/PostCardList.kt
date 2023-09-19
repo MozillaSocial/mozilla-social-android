17144bc1e45e449079ed30eaf4da92d99c311367
John Oberhauser <j.git-global@obez.io>
2023-09-19 11:35:23 -0500

    fixing padding issue

--- core/ui/src/main/java/org/mozilla/social/core/ui/postcard/PostCardList.kt
+++ core/ui/src/main/java/org/mozilla/social/core/ui/postcard/PostCardList.kt
@@ -72,7 +72,7 @@ fun PostCardList(
         LazyColumn(
             Modifier
                 .fillMaxSize()
-                .padding(start = 4.dp, end = 4.dp),
+                .padding(horizontal = 4.dp),
         ) {
 
             when (lazyingPagingItems.loadState.refresh) {
@@ -165,7 +165,7 @@ fun PostCardList(
     LazyColumn(
         Modifier
             .fillMaxSize()
-            .padding(start = 4.dp, end = 4.dp),
+            .padding(horizontal = 4.dp),
     ) {
         items(
             count = items.count(),
