9a3e3e8bd288f2e58ed3c85834374e9aa2c6dec7
John Oberhauser <j.git-global@obez.io>
2023-09-20 14:55:14 -0500

    new post polls

--- feature/post/src/main/java/org/mozilla/social/post/poll/PollDurationDropdown.kt
+++ feature/post/src/main/java/org/mozilla/social/post/poll/PollDurationDropdown.kt
@@ -10,9 +10,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import org.mozilla.social.core.designsystem.theme.MozillaSocialTheme
+import org.mozilla.social.feature.post.R
 
 @Composable
 fun PollDurationDropDown(
@@ -29,11 +32,11 @@ fun PollDurationDropDown(
     ) {
         Column {
             Text(
-                text = "Poll Duration",
+                text = stringResource(id = R.string.poll_duration),
                 fontSize = 12.sp
             )
             Text(
-                text = poll.pollDuration.label
+                text = poll.pollDuration.label.build(LocalContext.current)
             )
         }
     }
@@ -45,7 +48,7 @@ fun PollDurationDropDown(
         PollDuration.values().forEach { pollDuration ->
             DropdownMenuItem(
                 text = {
-                    Text(text = pollDuration.label)
+                    Text(text = pollDuration.label.build(LocalContext.current))
                 },
                 onClick = {
                     pollInteractions.onPollDurationSelected(pollDuration)
--- feature/post/src/main/java/org/mozilla/social/post/poll/PollStyle.kt
+++ feature/post/src/main/java/org/mozilla/social/post/poll/PollStyle.kt
@@ -1,8 +1,11 @@
 package org.mozilla.social.post.poll
 
+import org.mozilla.social.common.utils.StringFactory
+import org.mozilla.social.feature.post.R
+
 enum class PollStyle(
-    val label: String
+    val label: StringFactory
 ) {
-    SINGLE_CHOICE("Single choice"),
-    MULTIPLE_CHOICE("Multiple choice")
+    SINGLE_CHOICE(StringFactory.string(R.string.poll_style_single_choice)),
+    MULTIPLE_CHOICE(StringFactory.string(R.string.poll_style_multiple_choice))
 }
