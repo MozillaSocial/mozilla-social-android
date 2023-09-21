b94732c95adbf0adf45d9d8e1d7bedc312bcf0ee
Devota <daabel@mozilla.com>
2023-09-21 13:41:21 -0400

    Added side effects for callbacks from compose for login

--- feature/auth/src/main/java/org/mozilla/social/feature/auth/LoginScreen.kt
+++ feature/auth/src/main/java/org/mozilla/social/feature/auth/LoginScreen.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -38,7 +38,7 @@ fun LoginScreen(
     val context = LocalContext.current
 
     if (isSignedIn) {
-        LaunchedEffect(key1 = isSignedIn) {
+        SideEffect {
             navigateToLoggedInGraph()
         }
     }
