7fd83aa77b57e3da77d2ee4f163b2195bb6c5036
timc-mozilla <tchoh@mozilla.com>
2023-09-29 09:57:36 -0500

    glean simplified

--- app/src/main/java/org/mozilla/social/MainApplication.kt
+++ app/src/main/java/org/mozilla/social/MainApplication.kt
@@ -15,6 +15,7 @@ import org.koin.core.context.startKoin
 import org.koin.dsl.module
 import org.mozilla.social.common.commonModule
 import org.mozilla.social.core.analytics.glean.Analytics
+import org.mozilla.social.core.analytics.glean.GleanAnalytics
 import org.mozilla.social.core.analytics.glean.analyticsModule
 import org.mozilla.social.core.data.AuthCredentialObserver
 import org.mozilla.social.core.data.repositoryModule
--- core/analytics/src/main/java/org/mozilla/social/core/analytics/glean/Analytics.kt
+++ core/analytics/src/main/java/org/mozilla/social/core/analytics/glean/Analytics.kt
@@ -3,6 +3,7 @@ package org.mozilla.social.core.analytics.glean
 import android.content.Context
 import mozilla.telemetry.glean.BuildInfo
 import mozilla.telemetry.glean.Glean
+import org.mozilla.social.core.analytics.GleanMetrics.App
 import java.util.Calendar
 
 interface Analytics {
@@ -13,10 +14,16 @@ interface Analytics {
 class GleanAnalytics : Analytics {
     override fun initialize(context: Context) {
         val buildInfo = BuildInfo("1", "1", Calendar.getInstance())
+
+        Glean.setLogPings(true)
+        Glean.setDebugViewTag("Tim-Test")
+
         Glean.initialize(
             applicationContext = context,
             uploadEnabled = true,
             buildInfo = buildInfo
         )
+
+        App.opened.record()
     }
 }
