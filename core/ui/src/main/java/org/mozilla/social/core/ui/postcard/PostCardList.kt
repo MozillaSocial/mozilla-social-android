498b302ac62a74d89c4ddf30278d4ac50c35cd56
John Oberhauser <j.git-global@obez.io>
2023-10-02 15:29:15 -0500

    moving recs out of post card list

--- core/ui/src/main/java/org/mozilla/social/core/ui/postcard/PostCardList.kt
+++ core/ui/src/main/java/org/mozilla/social/core/ui/postcard/PostCardList.kt
@@ -1,22 +1,15 @@
 package org.mozilla.social.core.ui.postcard
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
 import androidx.paging.PagingData
@@ -28,37 +21,21 @@ import kotlinx.coroutines.flow.SharedFlow
 import org.mozilla.social.common.utils.StringFactory
 import org.mozilla.social.core.designsystem.component.MoSoDivider
 import org.mozilla.social.core.designsystem.component.MoSoToast
-import org.mozilla.social.core.ui.R
 import org.mozilla.social.core.ui.error.GenericError
 import org.mozilla.social.core.ui.pullrefresh.PullRefreshIndicator
 import org.mozilla.social.core.ui.pullrefresh.pullRefresh
 import org.mozilla.social.core.ui.pullrefresh.rememberPullRefreshState
-import org.mozilla.social.core.ui.recommendations.MoreInfoDialog
-import org.mozilla.social.core.ui.recommendations.RecommendationCarousel
-import org.mozilla.social.model.Recommendation
 
 @Composable
 fun PostCardList(
     feed: Flow<PagingData<PostCardUiState>>,
     errorToastMessage: SharedFlow<StringFactory>,
-    reccs: List<Recommendation>? = null,
     postCardInteractions: PostCardInteractions,
     enablePullToRefresh: Boolean = false,
     headerContent: @Composable () -> Unit = {},
 ) {
 
     val lazyingPagingItems: LazyPagingItems<PostCardUiState> = feed.collectAsLazyPagingItems()
-
-    val openAlertDialog = remember { mutableStateOf(false) }
-
-    if (openAlertDialog.value) {
-        MoreInfoDialog(
-            onDismissRequest = { openAlertDialog.value = false },
-            onConfirmation = { openAlertDialog.value = false },
-            dialogTitle = stringResource(id = R.string.feed_recommendations_why_am_i_seeing_this),
-            dialogText = stringResource(id = R.string.feed_recommendations_reason_you_are_seeing_this),
-        )
-    }
 
     val pullRefreshState = rememberPullRefreshState(
         refreshing = lazyingPagingItems.loadState.refresh == LoadState.Loading,
@@ -91,12 +68,6 @@ fun PostCardList(
             when (lazyingPagingItems.loadState.refresh) {
                 is LoadState.Error -> {} // handle the error outside the lazy column
                 else -> {
-                    reccs?.let {
-                        item {
-                            RecommendationCarousel(reccs = it) { openAlertDialog.value = true }
-                        }
-                    }
-
                     items(
                         count = lazyingPagingItems.itemCount,
                         key = lazyingPagingItems.itemKey { it.statusId }
