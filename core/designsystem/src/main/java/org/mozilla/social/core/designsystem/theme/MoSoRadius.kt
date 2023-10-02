4dd035143a5a2905b7cde7c1edf2d93d377b7659
Devota Aabel <daabel@mozilla.com>
2023-10-02 13:46:42 -0400

    [MOSOAND-89, MOSOAND-90] Added objects/values for radius and spacing (#160)

---
+++ core/designsystem/src/main/java/org/mozilla/social/core/designsystem/theme/MoSoRadius.kt
@@ -0,0 +1,10 @@
+package org.mozilla.social.core.designsystem.theme
+
+import androidx.compose.ui.unit.dp
+
+object MoSoRadius {
+    val sm = 4.dp
+    val md = 8.dp
+    val lg = 16.dp
+    val full = 360.dp
+}
