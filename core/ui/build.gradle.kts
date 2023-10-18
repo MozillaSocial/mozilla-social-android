0ba619806c1b3f4f442bfdaaa03921419969bc5d
John Oberhauser <j.git-global@obez.io>
2023-10-17 22:51:25 -0500

    setting video player aspect ratio

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(libs.androidx.media3.exoplayer)
     implementation(libs.androidx.media3.ui)
 
+    implementation(libs.jakewharton.timber)
 
     // compose
     implementation(platform(libs.androidx.compose.bom))
