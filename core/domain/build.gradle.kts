9fbf056041ac8b97fa2398c727e07b36ef8a6d12
Tim <111293209+timc-mozilla@users.noreply.github.com>
2023-10-24 14:29:00 -0500

    Feature analytics (#208)

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation(project(":core:model"))
     implementation(project(":core:database"))
     implementation(project(":core:common"))
+    implementation(project(":core:analytics"))
 
     implementation(libs.jakewharton.timber)
     implementation(libs.koin)
