74c84bbda8126b86c5918632fa9b861545cfdb91
timc-mozilla <tchoh@mozilla.com>
2023-11-02 16:24:57 -0500

    fixing build error

--- feature/hashtag/src/main/java/org/mozilla/social/feature/hashtag/HashTagModule.kt
+++ feature/hashtag/src/main/java/org/mozilla/social/feature/hashtag/HashTagModule.kt
@@ -14,6 +14,7 @@ val hashTagModule = module {
             accountIdFlow = get(),
             navigateTo = get(),
             openLink = get(),
+            analytics = get(),
         )
     }
 }
