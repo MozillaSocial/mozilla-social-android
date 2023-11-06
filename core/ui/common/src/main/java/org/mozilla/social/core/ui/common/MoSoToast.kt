969f6bfe69bdf5df5a0143af374e9bd98bb85375
John Oberhauser <j.git-global@obez.io>
2023-11-06 17:03:12 -0600

    cleanup

--- core/ui/common/src/main/java/org/mozilla/social/core/ui/common/MoSoToast.kt
+++ core/ui/common/src/main/java/org/mozilla/social/core/ui/common/MoSoToast.kt
@@ -2,7 +2,6 @@ package org.mozilla.social.core.ui.common
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.platform.LocalContext
 import kotlinx.coroutines.flow.SharedFlow
 import org.koin.compose.koinInject
 import org.mozilla.social.common.utils.StringFactory
@@ -13,8 +12,6 @@ fun MoSoErrorToast(
     toastMessage: SharedFlow<StringFactory>,
     showSnackbar: ShowSnackbar = koinInject(),
 ) {
-    val context = LocalContext.current
-
     LaunchedEffect(Unit) {
         toastMessage.collect {
             showSnackbar(it, isError = true)
