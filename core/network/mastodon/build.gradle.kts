82461194b12b75c35d8b9eed6fbfe4e22f142b76
Devota <daabel@mozilla.com>
2023-11-10 11:45:37 -0500

    Added network:mastodon module

---
+++ core/network/mastodon/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("org.mozilla.social.android.library")
+}
+
+android {
+    namespace = "org.mozilla.social.core.network.mastodon"
+}
+
+dependencies {
+    implementation(project(":core:common"))
+
+    api(libs.retrofit)
+
+    implementation(libs.square.okhttp)
+    implementation(libs.square.okhttp.logging)
+
+    implementation(libs.kotlinx.datetime)
+
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.koin.core)
+    implementation(libs.koin.androidx.compose)
+    implementation(libs.koin.android)
+
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.serialization.converter)
+}
