05f4de431b4e68af5b77c08eea7fe5b4759b0f0e
Devota <daabel@mozilla.com>
2023-11-10 13:18:05 -0500

    Created mozilla and mastodon repository modules

---
+++ core/repository/mozilla/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("org.mozilla.social.android.library")
+    id("org.mozilla.social.android.library.secrets")
+
+}
+
+android {
+    namespace = "org.mozilla.social.core.repository.mozilla"
+}
+
+dependencies {
+    implementation(project(":core:common"))
+    implementation(project(":core:network:mozilla"))
+    implementation(project(":core:network:mastodon"))
+    implementation(project(":core:datastore"))
+    implementation(project(":core:model"))
+    implementation(project(":core:database"))
+
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.datetime)
+
+    implementation(libs.retrofit)
+
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.koin.core)
+    implementation(libs.koin.androidx.compose)
+
+    implementation(libs.androidx.datastore)
+    implementation(libs.protobuf.kotlin.lite)
+
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room)
+}
