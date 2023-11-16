20057d2072a56cb6c5536f6ba79b1592e4fdf60b
Devota Aabel <daabel@mozilla.com>
2023-11-16 13:23:39 -0500

    Blocks and mutes list part 2 - Created and populated blocks list screen with api result (#285)
    I've been reading the [API guidelines for
    compose](https://android.googlesource.com/platform/frameworks/support/+/androidx-main/compose/docs/compose-component-api-guidelines.md#component_s-purpose),
    and I've tried to implement a few suggestions in this PR:
    - Created separate named styled text composables (see `SmallTextLabel`,
    `LargeTextTitle`, etc.) instead of manually always needing to pass in
    style to emulate "building blocks" described in docs
    - Started updating parameter order
    - Added modifier to all components (as the first optional param always)
    - Tried to give each component only one problem to solve
    - Updated `QuickAccountView` with a button slot (and other guidelines)

---
+++ feature/settings/src/main/java/org/mozilla/social/feature/settings/contentpreferences/blockedusers/BlockedUsersViewModel.kt
@@ -0,0 +1,33 @@
+package org.mozilla.social.feature.settings.contentpreferences.blockedusers
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import org.mozilla.social.core.model.Account
+import org.mozilla.social.core.repository.mastodon.AccountRepository
+import org.mozilla.social.core.repository.mastodon.BlocksRepository
+import org.mozilla.social.core.ui.common.account.quickview.toQuickViewUiState
+
+class BlockedUsersViewModel(
+    private val repository: BlocksRepository,
+    private val accountRepository: AccountRepository,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
+) : ViewModel() {
+
+    val blocks: Flow<List<Block>> = flow {
+        emit(repository.getBlocks().map { it.toBlock() })
+    }
+
+    // TODO@DA error handling and unblock
+    fun onBlockButtonClicked(accountId: String) {
+        viewModelScope.launch(ioDispatcher) { accountRepository.muteAccount(accountId) }
+    }
+}
+
+private fun Account.toBlock(): Block {
+    return Block(toQuickViewUiState())
+}
---
+++ feature/settings/src/main/java/org/mozilla/social/feature/settings/contentpreferences/mutedusers/MutedUsersSettingsViewModel.kt
@@ -0,0 +1,31 @@
+package org.mozilla.social.feature.settings.contentpreferences.mutedusers
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import org.mozilla.social.core.model.Account
+import org.mozilla.social.core.repository.mastodon.AccountRepository
+import org.mozilla.social.core.repository.mastodon.MutesRepository
+
+class MutedUsersSettingsViewModel(
+    private val repository: MutesRepository,
+    private val accountRepository: AccountRepository,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
+) : ViewModel() {
+
+    val mutes: Flow<List<Account>> = flow<List<Account>> {
+        emit(repository.getMutes())
+    }
+
+    // TODO@DA hook up
+    fun onMuteButtonClicked(
+        accountId: String,
+    ) {
+        viewModelScope.launch(ioDispatcher) { accountRepository.muteAccount(accountId) }
+    }
+}
+
