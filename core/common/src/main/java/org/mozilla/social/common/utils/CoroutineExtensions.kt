0cd188a6fed0d7716c2f3e11ca59721da5176479
Devota <daabel@mozilla.com>
2023-11-17 16:36:26 -0500

    Removed disabling of new line rule

--- core/common/src/main/java/org/mozilla/social/common/utils/CoroutineExtensions.kt
+++ core/common/src/main/java/org/mozilla/social/common/utils/CoroutineExtensions.kt
@@ -4,4 +4,5 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.supervisorScope
 
-fun <R> CoroutineScope.launchSupervisor(block: suspend CoroutineScope.() -> R) = launch { supervisorScope { block.invoke(this) } }
+fun <R> CoroutineScope.launchSupervisor(block: suspend CoroutineScope.() -> R) =
+    launch { supervisorScope { block.invoke(this) } }
