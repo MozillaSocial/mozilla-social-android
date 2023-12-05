d7924ee0487f22b86fe07062ba5ad2d316b4d2d6
John Oberhauser <j.git-global@obez.io>
2023-12-04 19:49:47 -0600

    pager

--- core/repository/mastodon/src/main/java/org/mozilla/social/core/repository/mastodon/FavoritesRepository.kt
+++ core/repository/mastodon/src/main/java/org/mozilla/social/core/repository/mastodon/FavoritesRepository.kt
@@ -1,6 +1,18 @@
 package org.mozilla.social.core.repository.mastodon
 
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.RemoteMediator
+import androidx.paging.map
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import org.mozilla.social.common.parseMastodonLinkHeader
+import org.mozilla.social.core.database.dao.FavoritesTimelineStatusDao
+import org.mozilla.social.core.database.model.statusCollections.FavoritesTimelineStatusWrapper
+import org.mozilla.social.core.database.model.statusCollections.toStatusWrapper
+import org.mozilla.social.core.model.Status
 import org.mozilla.social.core.model.paging.StatusPagingWrapper
 import org.mozilla.social.core.network.mastodon.FavoritesApi
 import org.mozilla.social.core.repository.mastodon.model.status.toExternalModel
@@ -8,6 +20,7 @@ import retrofit2.HttpException
 
 class FavoritesRepository(
     private val api: FavoritesApi,
+    private val dao: FavoritesTimelineStatusDao,
 ) {
     suspend fun getFavorites(
         olderThanId: String? = null,
@@ -30,4 +43,25 @@ class FavoritesRepository(
             pagingLinks = response.headers().get("link")?.parseMastodonLinkHeader(),
         )
     }
+
+    @ExperimentalPagingApi
+    fun getFavoritesPager(
+        remoteMediator: RemoteMediator<Int, FavoritesTimelineStatusWrapper>,
+        pageSize: Int = 20,
+        initialLoadSize: Int = 40,
+    ): Flow<PagingData<Status>> =
+        Pager(
+            config =
+            PagingConfig(
+                pageSize = pageSize,
+                initialLoadSize = initialLoadSize,
+            ),
+            remoteMediator = remoteMediator,
+        ) {
+            dao.favoritesTimelinePagingSource()
+        }.flow.map { pagingData ->
+            pagingData.map {
+                it.toStatusWrapper().toExternalModel()
+            }
+        }
 }
