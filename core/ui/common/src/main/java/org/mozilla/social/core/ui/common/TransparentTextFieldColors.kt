d90c16828098982be24bc524d442360fe0d1bc8a
John Oberhauser <j.git-global@obez.io>
2023-12-06 17:59:30 -0600

    fixing crash and theme issue

--- core/ui/common/src/main/java/org/mozilla/social/core/ui/common/TransparentTextFieldColors.kt
+++ core/ui/common/src/main/java/org/mozilla/social/core/ui/common/TransparentTextFieldColors.kt
@@ -3,10 +3,11 @@ package org.mozilla.social.core.ui.common
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import org.mozilla.social.core.ui.common.text.MoSoTextFieldDefaults
 
 @Composable
 fun transparentTextFieldColors() =
-    TextFieldDefaults.colors(
+    MoSoTextFieldDefaults.colors(
         focusedContainerColor = Color.Transparent,
         focusedIndicatorColor = Color.Transparent,
         unfocusedIndicatorColor = Color.Transparent,
