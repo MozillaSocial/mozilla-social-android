dd337e108e5eb55223dc505bf43e28cf8499dc96
John Oberhauser <j.git-global@obez.io>
2023-12-18 10:23:32 -0600

    test network call

--- feature/search/src/main/java/org/mozilla/social/search/SearchViewModel.kt
+++ feature/search/src/main/java/org/mozilla/social/search/SearchViewModel.kt
@@ -1,11 +1,18 @@
 package org.mozilla.social.search
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import org.mozilla.social.common.utils.edit
+import org.mozilla.social.core.model.SearchType
+import org.mozilla.social.core.repository.mastodon.SearchRepository
+import timber.log.Timber
 
-class SearchViewModel : ViewModel(), SearchInteractions {
+class SearchViewModel(
+    private val searchRepository: SearchRepository,
+) : ViewModel(), SearchInteractions {
 
     private val _uiState = MutableStateFlow(SearchUiState())
     val uiState = _uiState.asStateFlow()
@@ -17,7 +24,16 @@ class SearchViewModel : ViewModel(), SearchInteractions {
     }
 
     override fun onSearchClicked() {
-        TODO("Not yet implemented")
+        viewModelScope.launch {
+            try {
+                searchRepository.search(
+                    query = uiState.value.query,
+                    type = SearchType.Accounts,
+                )
+            } catch (e: Exception) {
+                Timber.e(e)
+            }
+        }
     }
 
     override fun onTabClicked(tab: SearchTab) {
