4ec3249c78a634c060f1f9ca3f995ec935581785
John Oberhauser <j.git-global@obez.io>
2023-12-22 11:36:20 -0600

    created use cases

--- core/repository/mastodon/src/main/java/org/mozilla/social/core/repository/mastodon/HashtagRepository.kt
+++ core/repository/mastodon/src/main/java/org/mozilla/social/core/repository/mastodon/HashtagRepository.kt
@@ -1,16 +1,39 @@
 package org.mozilla.social.core.repository.mastodon
 
+import org.mozilla.social.common.annotations.PreferUseCase
 import org.mozilla.social.core.database.dao.HashTagsDao
 import org.mozilla.social.core.model.HashTag
+import org.mozilla.social.core.network.mastodon.TagsApi
 import org.mozilla.social.core.repository.mastodon.model.hashtag.toDatabaseModel
+import org.mozilla.social.core.repository.mastodon.model.hashtag.toExternalModel
 
 class HashtagRepository(
-    private val hashTagsDao: HashTagsDao,
+    private val dao: HashTagsDao,
+    private val api: TagsApi,
 ) {
 
     fun insertAll(
         hashTags: List<HashTag>
-    ) {
-        hashTagsDao.upsertAll(hashTags.map { it.toDatabaseModel() })
-    }
+    ) = dao.upsertAll(hashTags.map { it.toDatabaseModel() })
+
+    fun insert(
+        hashTag: HashTag,
+    ) = dao.upsert(hashTag.toDatabaseModel())
+
+    @PreferUseCase
+    suspend fun updateFollowing(
+        hashTag: String,
+        isFollowing: Boolean,
+    ) = dao.updateFollowing(
+        hashTag,
+        isFollowing,
+    )
+
+    suspend fun followHashTag(
+        hashTag: String,
+    ): HashTag = api.followHashTag(hashTag).toExternalModel()
+
+    suspend fun unfollowHashTag(
+        hashTag: String,
+    ): HashTag = api.unfollowHashTag(hashTag).toExternalModel()
 }
