d33967e991c1651dbced4816073fc726f2dee7da
John Oberhauser <j.git-global@obez.io>
2024-01-04 12:54:24 -0600

    apk releases

--- ci/nightly-build.sh
+++ ci/nightly-build.sh
@@ -4,8 +4,12 @@
 set -e
 
 BUILD_OUTPUT_DIR="app/build/outputs/bundle/nightly"
+
 UNSIGNED_AAB="$BUILD_OUTPUT_DIR/app-nightly.aab"
 SIGNED_AAB="secrets/mozilla-social-nightly.aab"
+
+UNSIGNED_APK="$BUILD_OUTPUT_DIR/app-nightly.apk"
+SIGNED_APK="secrets/mozilla-social-nightly.apk"
 
 SECRET_ENV="secrets/secret-environment-variables.sh"
 
@@ -17,10 +21,13 @@ fi
 echo "Initializing secrets…"
 source "$SECRET_ENV"
 
-echo "Building…"
+echo "Building AAB…"
 ./gradlew clean :app:bundleNightly
 
-echo "Signing…"
+echo "Building APK…"
+./gradlew :app:assembleNightly
+
+echo "Signing AAB…"
 curl \
   -F "input=@$UNSIGNED_AAB" \
   -o "$SIGNED_AAB" \
@@ -28,5 +35,14 @@ curl \
   --fail \
   https://edge.prod.autograph.services.mozaws.net/sign
 
+echo "Signing APK…"
+curl \
+  -F "input=@$UNSIGNED_APK" \
+  -o "$SIGNED_APK" \
+  -H "Authorization: $AUTOGRAPH_EDGE_NIGHTLY_CLIENT_TOKEN" \
+  --fail \
+  https://edge.prod.autograph.services.mozaws.net/sign
+
 echo "Done!"
 echo "The signed AAB is at $SIGNED_AAB"
+echo "The signed APK is at $SIGNED_APK"
