b40b86925c9670ff7fb10f426e485c2f9919aae0
John Oberhauser <j.git-global@obez.io>
2024-01-05 13:00:52 -0600

    fixing screen padding issues

--- feature/settings/src/main/java/org/mozilla/social/feature/settings/SettingsScreen.kt
+++ feature/settings/src/main/java/org/mozilla/social/feature/settings/SettingsScreen.kt
@@ -1,9 +1,10 @@
 package org.mozilla.social.feature.settings
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,6 +16,7 @@ import org.koin.androidx.compose.koinViewModel
 import org.mozilla.social.common.Version
 import org.mozilla.social.core.designsystem.icon.MoSoIcons
 import org.mozilla.social.core.navigation.navigationModule
+import org.mozilla.social.core.ui.common.MoSoSurface
 import org.mozilla.social.core.ui.common.utils.PreviewTheme
 import org.mozilla.social.feature.settings.ui.SettingsColumn
 import org.mozilla.social.feature.settings.ui.SettingsSection
@@ -36,37 +38,44 @@ fun SettingsScreen(
     onPrivacyClicked: () -> Unit,
     onAboutClicked: () -> Unit,
 ) {
-    SettingsColumn(
-        modifier = Modifier.fillMaxSize(),
-        title = stringResource(id = R.string.settings_title)
-    ) {
-        SettingsSection(
-            title = stringResource(id = R.string.account_settings_title),
-            iconPainter = MoSoIcons.identificationCard(),
-            onClick = onAccountClicked,
-        )
-        SettingsSection(
-            title = stringResource(id = R.string.content_preferences_title),
-            iconPainter = MoSoIcons.listChecks(),
-            onClick = onContentPreferencesClicked,
-        )
-        SettingsSection(
-            title = stringResource(id = R.string.privacy_settings_title),
-            iconPainter = MoSoIcons.lockKey(),
-            onClick = onPrivacyClicked,
-        )
-        SettingsSection(
-            title = stringResource(id = R.string.about_settings_title),
-            iconPainter = MoSoIcons.info(),
-            onClick = onAboutClicked,
-        )
-
-        Spacer(modifier = Modifier.weight(1f))
-        Text(
+    MoSoSurface {
+        Box(
             modifier = Modifier
-                .align(Alignment.CenterHorizontally),
-            text = "v${Version.name}.${Version.code}",
-        )
+                .fillMaxSize()
+                .systemBarsPadding()
+        ) {
+            SettingsColumn(
+                title = stringResource(id = R.string.settings_title)
+            ) {
+                SettingsSection(
+                    title = stringResource(id = R.string.account_settings_title),
+                    iconPainter = MoSoIcons.identificationCard(),
+                    onClick = onAccountClicked,
+                )
+                SettingsSection(
+                    title = stringResource(id = R.string.content_preferences_title),
+                    iconPainter = MoSoIcons.listChecks(),
+                    onClick = onContentPreferencesClicked,
+                )
+                SettingsSection(
+                    title = stringResource(id = R.string.privacy_settings_title),
+                    iconPainter = MoSoIcons.lockKey(),
+                    onClick = onPrivacyClicked,
+                )
+                SettingsSection(
+                    title = stringResource(id = R.string.about_settings_title),
+                    iconPainter = MoSoIcons.info(),
+                    onClick = onAboutClicked,
+                )
+
+                Spacer(modifier = Modifier.weight(1f))
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally),
+                    text = "v${Version.name}.${Version.code}",
+                )
+            }
+        }
     }
 }
 
