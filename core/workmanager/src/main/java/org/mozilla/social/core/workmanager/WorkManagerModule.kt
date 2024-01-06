9d311d94619bece1d18ff294ee85ab2e45f9c21d
John Oberhauser <j.git-global@obez.io>
2024-01-06 15:59:01 -0600

    cleanup

--- core/workmanager/src/main/java/org/mozilla/social/core/workmanager/WorkManagerModule.kt
+++ core/workmanager/src/main/java/org/mozilla/social/core/workmanager/WorkManagerModule.kt
@@ -2,11 +2,9 @@ package org.mozilla.social.core.workmanager
 
 import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.workmanager.dsl.worker
-import org.koin.androidx.workmanager.dsl.workerOf
 import org.koin.dsl.module
 
 val workManagerModule = module {
-    workerOf(::DatabasePurgeWorker)
     worker {
         DatabasePurgeWorker(
             context = androidApplication(),
