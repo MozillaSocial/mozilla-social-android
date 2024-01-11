4fd48eb87a7f294aebfa2b974c7fb44268daaab1
John Oberhauser <j.git-global@obez.io>
2024-01-11 09:47:53 -0600

    adding ui notifications module

---
+++ core/ui/notifications/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("org.mozilla.social.android.library")
+    id("org.mozilla.social.android.library.compose")
+}
+
+android {
+    namespace = "org.mozilla.social.core.ui.notifications"
+}
+
+dependencies {
+    implementation(project(":core:model"))
+    implementation(project(":core:designsystem"))
+    implementation(project(":core:ui:common"))
+}
