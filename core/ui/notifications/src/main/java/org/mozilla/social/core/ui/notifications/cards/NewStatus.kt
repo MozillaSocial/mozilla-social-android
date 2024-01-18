0c1dd7450c34884488aa64fd391e70d388d3c83e
John Oberhauser <j.git-global@obez.io>
2024-01-18 10:55:30 -0600

    cleanup

--- core/ui/notifications/src/main/java/org/mozilla/social/core/ui/notifications/cards/NewStatus.kt
+++ core/ui/notifications/src/main/java/org/mozilla/social/core/ui/notifications/cards/NewStatus.kt
@@ -1,11 +1,7 @@
 package org.mozilla.social.core.ui.notifications.cards
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import org.mozilla.social.common.utils.StringFactory
 import org.mozilla.social.core.ui.common.utils.PreviewTheme
 import org.mozilla.social.core.ui.htmlcontent.HtmlContentInteractions
@@ -15,16 +11,6 @@ import org.mozilla.social.core.ui.notifications.NotificationUiState
 import org.mozilla.social.core.ui.poll.PollInteractions
 import org.mozilla.social.core.ui.postcard.PostContent
 import org.mozilla.social.core.ui.postcard.PostContentUiState
-
-@Composable
-fun NewStatusNotification(
-    uiState: NotificationUiState.NewStatus
-) {
-    Text(
-        modifier = Modifier.padding(8.dp),
-        text = "NewStatus ${uiState.id}"
-    )
-}
 
 @Composable
 internal fun NewStatusNotificationContent(
