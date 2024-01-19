7d5eaed3d1f078c21c9948b3014657696127ec9e
John Oberhauser <j.git-global@obez.io>
2024-01-19 16:07:41 -0600

    thread list

--- core/ui/postcard/src/main/java/org/mozilla/social/core/ui/postcard/PostCardList.kt
+++ core/ui/postcard/src/main/java/org/mozilla/social/core/ui/postcard/PostCardList.kt
@@ -1,12 +1,7 @@
 package org.mozilla.social.core.ui.postcard
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.itemKey
 import org.mozilla.social.core.ui.common.divider.MoSoDivider
@@ -20,45 +15,30 @@ fun LazyListScope.postListContent(
         key = lazyPagingItems.itemKey { it.statusId },
     ) { index ->
         lazyPagingItems[index]?.let { item ->
-            PostCard(
-                post = item,
+            PostCardListItem(
+                uiState = item,
                 postCardInteractions = postCardInteractions,
+                index = index,
+                itemCount = lazyPagingItems.itemCount,
             )
-            if (index < lazyPagingItems.itemCount) {
-                MoSoDivider()
-            }
         }
     }
 }
 
-/**
- * @param threadId if viewing a thread, pass the threadId in to prevent
- * the user from being able to click on the same status as the thread's root status
- */
 @Composable
-fun PostCardList(
-    items: List<PostCardUiState>,
+fun PostCardListItem(
+    uiState: PostCardUiState,
     postCardInteractions: PostCardInteractions,
+    index: Int,
+    itemCount: Int,
     threadId: String? = null,
 ) {
-    LazyColumn(
-        Modifier
-            .fillMaxSize()
-            .padding(horizontal = 4.dp),
-    ) {
-        items(
-            count = items.count(),
-            key = { items[it].statusId },
-        ) { index ->
-            val item = items[index]
-            PostCard(
-                post = item,
-                postCardInteractions = postCardInteractions,
-                threadId = threadId,
-            )
-            if (index < items.count()) {
-                MoSoDivider()
-            }
-        }
+    PostCard(
+        post = uiState,
+        postCardInteractions = postCardInteractions,
+        threadId = threadId,
+    )
+    if (index < itemCount) {
+        MoSoDivider()
     }
 }
