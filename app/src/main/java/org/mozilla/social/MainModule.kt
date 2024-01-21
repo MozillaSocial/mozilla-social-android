a0cc4eb53861dbadb9d57c28e534ea14aa6aa99a
John Oberhauser <j.git-global@obez.io>
2024-01-21 17:41:58 -0600

    core module tests

--- app/src/main/java/org/mozilla/social/MainModule.kt
+++ app/src/main/java/org/mozilla/social/MainModule.kt
@@ -2,8 +2,16 @@ package org.mozilla.social
 
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import org.mozilla.social.core.datastore.dataStoreModule
+import org.mozilla.social.core.navigation.navigationModule
+import org.mozilla.social.core.usecase.mastodon.mastodonUsecaseModule
 
-val mainModule =
-    module {
-        viewModel { MainViewModel(get(), get(), get(), get()) }
-    }
+val mainModule = module {
+    includes(
+        mastodonUsecaseModule,
+        navigationModule,
+        dataStoreModule,
+    )
+    
+    viewModel { MainViewModel(get(), get(), get(), get()) }
+}
