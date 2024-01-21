90fa3e82937e6a746decd3e1a04de6b80681b252
John Oberhauser <j.git-global@obez.io>
2024-01-21 16:54:37 -0600

    account and auth tests

---
+++ feature/account/src/test/kotlin/org/mozilla/social/feature/account/CheckModulesTest.kt
@@ -0,0 +1,25 @@
+package org.mozilla.social.feature.account
+
+import android.content.Context
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import org.koin.core.annotation.KoinExperimentalAPI
+import org.koin.test.KoinTest
+import org.koin.test.verify.verify
+import org.mozilla.social.core.model.AccountTimelineType
+import kotlin.test.Test
+
+class CheckModulesTest : KoinTest {
+    @OptIn(KoinExperimentalAPI::class)
+    @Test
+    fun checkAllModules() {
+        accountModule.verify(
+            extraTypes = listOf(
+                Context::class,
+                CoroutineDispatcher::class,
+                CoroutineScope::class,
+                AccountTimelineType::class,
+            ),
+        )
+    }
+}
