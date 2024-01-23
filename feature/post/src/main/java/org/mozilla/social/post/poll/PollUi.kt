6bd08b81423d8cc113bd43762cbf5c58323aa0ec
John Oberhauser <j.git-global@obez.io>
2024-01-23 12:23:12 -0600

    adding gradiant to end of poll bar

--- feature/post/src/main/java/org/mozilla/social/post/poll/PollUi.kt
+++ feature/post/src/main/java/org/mozilla/social/post/poll/PollUi.kt
@@ -1,13 +1,16 @@
 package org.mozilla.social.post.poll
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -22,11 +25,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.mozilla.social.core.designsystem.icon.MoSoIcons
+import org.mozilla.social.core.designsystem.theme.FirefoxColor
 import org.mozilla.social.core.designsystem.theme.MoSoTheme
 import org.mozilla.social.core.designsystem.utils.NoRipple
 import org.mozilla.social.core.ui.common.MoSoCheckBox
@@ -125,50 +131,69 @@ internal fun PollBar(
             .height(56.dp)
     ) {
         MoSoDivider()
-        Row(
+        Box(
             modifier = Modifier
                 .weight(1f)
-                .horizontalScroll(rememberScrollState()),
-            verticalAlignment = Alignment.CenterVertically,
         ) {
-            Spacer(modifier = Modifier.width(16.dp))
-
-            IconButton(
+            Row(
                 modifier = Modifier
-                    .border(
-                        width = 1.dp,
-                        color = MoSoTheme.colors.borderPrimary,
-                        shape = CircleShape,
-                    )
-                    .size(32.dp),
-                onClick = pollInteractions::onNewPollClicked
+                    .fillMaxHeight()
+                    .horizontalScroll(rememberScrollState()),
+                verticalAlignment = Alignment.CenterVertically,
             ) {
-                Icon(
-                    modifier = Modifier.size(16.dp),
-                    painter = MoSoIcons.x(),
-                    contentDescription = stringResource(id = R.string.close_poll_content_description)
+                Spacer(modifier = Modifier.width(16.dp))
+
+                IconButton(
+                    modifier = Modifier
+                        .border(
+                            width = 1.dp,
+                            color = MoSoTheme.colors.borderPrimary,
+                            shape = CircleShape,
+                        )
+                        .size(32.dp),
+                    onClick = pollInteractions::onNewPollClicked
+                ) {
+                    Icon(
+                        modifier = Modifier.size(16.dp),
+                        painter = MoSoIcons.x(),
+                        contentDescription = stringResource(id = R.string.close_poll_content_description)
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(24.dp))
+
+                PollDurationDropDown(
+                    pollUiState = pollUiState,
+                    pollInteractions = pollInteractions,
                 )
+
+                Spacer(modifier = Modifier.width(32.dp))
+
+                PollStyleDropDown(
+                    pollUiState = pollUiState,
+                    pollInteractions = pollInteractions,
+                )
+
+                Spacer(modifier = Modifier.width(32.dp))
+
+                HideUntilEndCheckbox(pollUiState = pollUiState, pollInteractions = pollInteractions)
+
+                Spacer(modifier = Modifier.width(16.dp))
             }
-
-            Spacer(modifier = Modifier.width(24.dp))
-
-            PollDurationDropDown(
-                pollUiState = pollUiState,
-                pollInteractions = pollInteractions,
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(50.dp)
+                    .align(Alignment.CenterEnd)
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                MoSoTheme.colors.layer1,
+                            )
+                        )
+                    )
             )
-
-            Spacer(modifier = Modifier.width(32.dp))
-
-            PollStyleDropDown(
-                pollUiState = pollUiState,
-                pollInteractions = pollInteractions,
-            )
-
-            Spacer(modifier = Modifier.width(32.dp))
-
-            HideUntilEndCheckbox(pollUiState = pollUiState, pollInteractions = pollInteractions)
-
-            Spacer(modifier = Modifier.width(16.dp))
         }
     }
 }
