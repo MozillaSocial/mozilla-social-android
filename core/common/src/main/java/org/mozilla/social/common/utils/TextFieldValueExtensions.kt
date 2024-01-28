d3b428cbfda5e5daa342ac62cdc0c4874bbe91e6
John Oberhauser <j.git-global@obez.io>
2024-01-27 22:40:24 -0600

    fixing issues

--- core/common/src/main/java/org/mozilla/social/common/utils/TextFieldValueExtensions.kt
+++ core/common/src/main/java/org/mozilla/social/common/utils/TextFieldValueExtensions.kt
@@ -6,32 +6,37 @@ import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.withStyle
+import kotlin.math.min
 
 /**
  * @return the account string a user is typing if they are typing one, otherwise null
  */
-fun TextFieldValue.accountText(): String? = findSymbolText('@')
+fun TextFieldValue.accountText(): String? = findSymbolTextAtCursor('@')
 
 /**
  * @return the hashtag string a user is typing if they are typing one, otherwise null
  */
-fun TextFieldValue.hashtagText(): String? = findSymbolText('#')
+fun TextFieldValue.hashtagText(): String? = findSymbolTextAtCursor('#')
 
 /**
  * @return the string between a symbol and the cursor a user is typing if they are typing one, otherwise null
  */
-fun TextFieldValue.findSymbolText(symbol: Char): String? {
+fun TextFieldValue.findSymbolTextAtCursor(symbol: Char): String? {
     if (!text.contains(symbol) || selection.start != selection.end) return null
-    val firstSpaceIndexAfterCursor = text.indexOf(' ', startIndex = selection.end)
+
+    val firstSpaceOrNewLineIndexAfterCursor = getFirstSpaceOrNewLineIndexAfterCursor(
+        text = text,
+        cursorIndex = selection.end,
+    )
     val textBeforeSpaceOrEnd =
-        if (firstSpaceIndexAfterCursor != -1) {
-            text.substring(0, firstSpaceIndexAfterCursor)
+        if (firstSpaceOrNewLineIndexAfterCursor != -1) {
+            text.substring(0, firstSpaceOrNewLineIndexAfterCursor)
         } else {
             text
         }
     if (!textBeforeSpaceOrEnd.contains(symbol)) return null
     val textBetweenSymbolAndCursor = textBeforeSpaceOrEnd.substringAfterLast(symbol)
-    return if (!textBetweenSymbolAndCursor.contains(" ")) {
+    return if (!textBetweenSymbolAndCursor.contains(' ') && !textBetweenSymbolAndCursor.contains('\n')) {
         textBetweenSymbolAndCursor
     } else {
         null
@@ -47,29 +52,39 @@ fun TextFieldValue.replaceSymbolText(
     newText: String,
 ): TextFieldValue {
     if (!text.contains(symbol)) return this
-    val firstSpaceIndexAfterCursor = text.indexOf(' ', startIndex = selection.end)
+    val firstSpaceOrNewLineIndexAfterCursor = getFirstSpaceOrNewLineIndexAfterCursor(
+        text = text,
+        cursorIndex = selection.end,
+    )
     val textBeforeSpaceOrEnd =
-        if (firstSpaceIndexAfterCursor != -1) {
-            text.substring(0, firstSpaceIndexAfterCursor)
+        if (firstSpaceOrNewLineIndexAfterCursor != -1) {
+            text.substring(0, firstSpaceOrNewLineIndexAfterCursor)
         } else {
             text
         }
     if (!textBeforeSpaceOrEnd.contains(symbol) ||
-        textBeforeSpaceOrEnd.substringAfterLast(symbol).contains(" ")
+        textBeforeSpaceOrEnd.substringAfterLast(symbol).contains(' ') ||
+        textBeforeSpaceOrEnd.substringAfterLast(symbol).contains('\n')
     ) {
         return this
     }
     val rangeStart = textBeforeSpaceOrEnd.lastIndexOf(symbol) + 1
     val rangeEnd =
-        if (firstSpaceIndexAfterCursor != -1) {
-            firstSpaceIndexAfterCursor
+        if (firstSpaceOrNewLineIndexAfterCursor != -1) {
+            firstSpaceOrNewLineIndexAfterCursor
         } else {
             text.length - 1
         }
+    // if there is a newline at the end, make sure to add it back in
+    val finalChar = if (text[rangeEnd] == '\n') {
+        '\n'
+    } else {
+        ""
+    }
     val finalText =
         text.replaceRange(
             rangeStart..rangeEnd,
-            "$newText ",
+            "$newText $finalChar",
         )
     return copy(
         text = finalText,
@@ -91,7 +106,8 @@ fun buildAnnotatedStringWithSymbols(
         var searchingForSymbol = true // if false then we are searching for a space
         text.forEachIndexed { index, character ->
             if (searchingForSymbol) {
-                if ((index == 0 || text[index - 1] == ' ') && symbols.contains(character)) {
+                val isStartOfWord = index == 0 || text[index - 1] == ' ' || text[index - 1] == '\n'
+                if (isStartOfWord && symbols.contains(character)) {
                     withStyle(style) {
                         append(character)
                     }
@@ -103,10 +119,27 @@ fun buildAnnotatedStringWithSymbols(
                 withStyle(style) {
                     append(character)
                 }
-                if (character == ' ') {
+                if (character == ' ' || character == '\n') {
                     searchingForSymbol = true
                 }
             }
         }
+    }
+}
+
+private fun getFirstSpaceOrNewLineIndexAfterCursor(
+    text: String,
+    cursorIndex: Int,
+): Int {
+    val firstSpaceIndexAfterCursor = text.indexOf(' ', startIndex = cursorIndex)
+    val firstNewLineIndexAfterCursor = text.indexOf('\n', startIndex = cursorIndex)
+
+    return when {
+        firstSpaceIndexAfterCursor == -1 -> firstNewLineIndexAfterCursor
+        firstNewLineIndexAfterCursor == -1 -> firstSpaceIndexAfterCursor
+        else -> min(
+            firstSpaceIndexAfterCursor,
+            firstNewLineIndexAfterCursor
+        )
     }
 }
