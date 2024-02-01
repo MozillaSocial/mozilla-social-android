d85b591fc2f83d42a672914d78283039afebb79e
John Oberhauser <j.git-global@obez.io>
2024-01-31 18:10:02 -0600

    detekt

--- app/src/main/java/org/mozilla/social/ui/AppState.kt
+++ app/src/main/java/org/mozilla/social/ui/AppState.kt
@@ -200,6 +200,7 @@ class AppState(
         if (!mainNavController.popBackStack()) tabbedNavController?.popBackStack()
     }
 
+    @Suppress("CyclomaticComplexMethod")
     private fun navigate(navDestination: NavigationDestination) {
         Timber.d("NAVIGATION consuming $navDestination")
         with(navDestination) {
