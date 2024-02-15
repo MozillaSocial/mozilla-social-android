d0a4f8df486252a78bc72765eeb3457c865c42e1
JohnOberhauser <14130581+JohnOberhauser@users.noreply.github.com>
2024-02-15 16:47:57 -0600

    Cleaning up actions (#3)
    * cleaning up actions
    
    * removing glean probe scraper
    
    * removing more moz stuff
    
    ---------
    
    Co-authored-by: John Oberhauser <j.git-global@obez.io>

--- ci/nightly-build.sh
+++ ci/nightly-build.sh
@@ -5,12 +5,12 @@ set -e
 
 BUNDLE_OUTPUT_DIR="app/build/outputs/bundle/nightly"
 UNSIGNED_AAB="$BUNDLE_OUTPUT_DIR/app-nightly.aab"
-SIGNED_AAB="secrets/mozilla-social-nightly.aab"
+SIGNED_AAB="secrets/firefly-nightly.aab"
 
 APK_OUTPUT_DIR="app/build/outputs/apk/nightly"
 UNSIGNED_APK="$APK_OUTPUT_DIR/app-nightly-unsigned.apk"
 ALIGNED_APK="$APK_OUTPUT_DIR/app-nightly-unsigned-aligned.apk"
-SIGNED_APK="secrets/mozilla-social-nightly.apk"
+SIGNED_APK="secrets/firefly-nightly.apk"
 
 BUILD_TOOLS_DIR="$ANDROID_HOME/build-tools/33.0.1"
 SECRET_ENV="secrets/secret-environment-variables.sh"
@@ -32,21 +32,22 @@ echo "Building APK…"
 echo "Aligning APK…"
 "$BUILD_TOOLS_DIR/zipalign" -p 4 "$UNSIGNED_APK" "$ALIGNED_APK"
 
-echo "Signing AAB…"
-curl \
-  -F "input=@$UNSIGNED_AAB" \
-  -o "$SIGNED_AAB" \
-  -H "Authorization: $AUTOGRAPH_EDGE_NIGHTLY_CLIENT_TOKEN" \
-  --fail \
-  https://edge.prod.autograph.services.mozaws.net/sign
+# TODO: sign APK and AAB
+#echo "Signing AAB…"
+#curl \
+#  -F "input=@$UNSIGNED_AAB" \
+#  -o "$SIGNED_AAB" \
+#  -H "Authorization: $AUTOGRAPH_EDGE_NIGHTLY_CLIENT_TOKEN" \
+#  --fail \
+#  https://edge.prod.autograph.services.mozaws.net/sign
 
-echo "Signing APK…"
-curl \
-  -F "input=@$ALIGNED_APK" \
-  -o "$SIGNED_APK" \
-  -H "Authorization: $AUTOGRAPH_EDGE_NIGHTLY_CLIENT_TOKEN" \
-  --fail \
-  https://edge.prod.autograph.services.mozaws.net/sign
+#echo "Signing APK…"
+#curl \
+#  -F "input=@$ALIGNED_APK" \
+#  -o "$SIGNED_APK" \
+#  -H "Authorization: $AUTOGRAPH_EDGE_NIGHTLY_CLIENT_TOKEN" \
+#  --fail \
+#  https://edge.prod.autograph.services.mozaws.net/sign
 
 echo "Done!"
 echo "The signed AAB is at $SIGNED_AAB"
--- secrets/decrypt.sh
+++ secrets/decrypt.sh
@@ -6,12 +6,10 @@ decrypt() {
 }
 
 if [[ -z "$GPG_KEY" ]]; then
-  read -p "Mozilla Social Android GPG key: " -r -s
+  read -p "Firefly GPG key: " -r -s
   echo # (optional) move to a new line
   GPG_KEY="$REPLY"
 fi
 
 decrypt "secret.properties"
-decrypt "../app/sentry.properties"
 decrypt "secret-environment-variables.sh" && chmod +x secret-environment-variables.sh
-decrypt "boxwood-axon-825-ed7aa5764ee6.json"
