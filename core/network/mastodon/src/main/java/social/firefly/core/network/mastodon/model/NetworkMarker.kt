f1a2868a60a4d26955b02b0be2ffd0a3301d655f
John Oberhauser <j.git-global@obez.io>
2024-04-15 13:48:06 -0500

    adding markers api

--- core/network/mastodon/src/main/java/social/firefly/core/network/mastodon/model/NetworkMarker.kt
+++ core/network/mastodon/src/main/java/social/firefly/core/network/mastodon/model/NetworkMarker.kt
@@ -1,16 +1,22 @@
 package social.firefly.core.network.mastodon.model
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
 /**
  * Marks the user's current position in their timelines,
  * to synchronize and restore it across devices.
  */
+@Serializable
 data class NetworkMarker(
     /**
      * Home timeline marker.
      */
+    @SerialName("home")
     val home: NetworkMarkerProperties,
     /**
      * Notifications timeline marker.
      */
+    @SerialName("notifications")
     val notifications: NetworkMarkerProperties,
 )
--- core/network/mastodon/src/main/java/social/firefly/core/network/mastodon/model/NetworkMarkerProperties.kt
+++ core/network/mastodon/src/main/java/social/firefly/core/network/mastodon/model/NetworkMarkerProperties.kt
@@ -1,21 +1,27 @@
 package social.firefly.core.network.mastodon.model
 
 import kotlinx.datetime.Instant
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
 /**
  * Marks the current reading position on a specific timeline.
  */
+@Serializable
 data class NetworkMarkerProperties(
     /**
      * ID of the last read item.
      */
+    @SerialName("last_read_id")
     val lastReadId: String,
     /**
      * Date at which this marker was updated.
      */
+    @SerialName("updated_at")
     val updatedAt: Instant,
     /**
      * Used for locking to prevent write conflicts.
      */
+    @SerialName("version")
     val version: Long,
 )
