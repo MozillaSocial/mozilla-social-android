483ca5c367b5d2d9a5b368e012738ef60030098b
John Oberhauser <j.git-global@obez.io>
2024-04-20 16:07:55 -0500

    bottom bar refactors

--- core/ui/postcard/src/main/java/social/firefly/core/ui/postcard/PostCard.kt
+++ core/ui/postcard/src/main/java/social/firefly/core/ui/postcard/PostCard.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,6 +23,7 @@ import social.firefly.common.utils.timeSinceNow
 import social.firefly.core.designsystem.utils.NoRipple
 import social.firefly.core.ui.common.TransparentNoTouchOverlay
 import social.firefly.core.ui.common.utils.PreviewTheme
+import social.firefly.core.ui.common.utils.getMaxWidth
 import social.firefly.core.ui.postcard.components.Avatar
 import social.firefly.core.ui.postcard.components.BottomRow
 import social.firefly.core.ui.postcard.components.DepthLines
@@ -101,7 +103,6 @@ private fun Post(
             )
 
             BottomRow(
-                modifier = Modifier,
                 post = post,
                 postCardInteractions = postCardInteractions,
             )
@@ -230,6 +231,54 @@ private fun PostCardPreviewWithDepth() {
                 ),
                 depthLinesUiState = DepthLinesUiState(
                     postDepth = 2,
+                    depthLines = listOf(
+                        0,
+                        1,
+                        2,
+                    ),
+                    startingDepth = 0,
+                ),
+            ),
+            postCardInteractions = PostCardInteractionsNoOp,
+        )
+    }
+}
+
+@Suppress("MagicNumber", "MaxLineLength")
+@Preview
+@Composable
+private fun PostCardPreviewWithHighDepth() {
+    PreviewTheme {
+        PostCard(
+            post = PostCardUiState(
+                statusId = "",
+                topRowMetaDataUiState = null,
+                mainPostCardUiState = MainPostCardUiState(
+                    url = "",
+                    username = "Cool guy",
+                    profilePictureUrl = "",
+                    postTimeSince = Instant.fromEpochMilliseconds(1695308821000L).timeSinceNow(),
+                    accountName = StringFactory.literal("coolguy"),
+                    replyCount = "4",
+                    boostCount = "300k",
+                    favoriteCount = "4.4m",
+                    statusId = "",
+                    userBoosted = false,
+                    isFavorited = false,
+                    accountId = "",
+                    isBeingDeleted = false,
+                    postContentUiState = PostContentUiState(
+                        pollUiState = null,
+                        statusTextHtml = "<p><span class=\"h-card\"><a href=\"https://mozilla.social/@obez\" class=\"u-url mention\" rel=\"nofollow noopener noreferrer\" target=\"_blank\">@<span>obez</span></a></span> This is a text status.  Here is the text and that is all I have to say about that.</p>",
+                        mediaAttachments = emptyList(),
+                        mentions = emptyList(),
+                        previewCard = null,
+                        contentWarning = "",
+                    ),
+                    dropDownOptions = listOf(),
+                ),
+                depthLinesUiState = DepthLinesUiState(
+                    postDepth = 15,
                     depthLines = listOf(
                         0,
                         1,
