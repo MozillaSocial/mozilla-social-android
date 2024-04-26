188848d5ee530760fe9dfb5b2f0a3c249702ebe3
John Oberhauser <j.git-global@obez.io>
2024-04-26 11:12:11 -0500

    fixing settings screen bottom padding

--- feature/settings/src/main/java/social/firefly/feature/settings/SettingsScreen.kt
+++ feature/settings/src/main/java/social/firefly/feature/settings/SettingsScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.koin.androidx.compose.koinViewModel
 import social.firefly.common.Version
 import social.firefly.core.designsystem.icon.FfIcons
@@ -47,7 +48,8 @@ fun SettingsScreen(
         ) {
             SettingsColumn(
                 modifier = Modifier
-                    .padding(horizontal = FfSpacing.md),
+                    .padding(horizontal = FfSpacing.md)
+                    .padding(bottom = 16.dp),
                 title = stringResource(id = R.string.settings_title)
             ) {
                 SettingsSection(
