637996a6414f1fa23f19a3555f4929a1609407e1
John Oberhauser <j.git-global@obez.io>
2024-05-13 17:17:03 -0500

    making everything look nice

--- core/ui/chooseAccount/src/main/kotlin/social/firefly/core/ui/chooseAccount/ChooseAccountDialog.kt
+++ core/ui/chooseAccount/src/main/kotlin/social/firefly/core/ui/chooseAccount/ChooseAccountDialog.kt
@@ -5,24 +5,30 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.AsyncImage
 import kotlinx.coroutines.flow.flowOf
 import org.koin.androidx.compose.koinViewModel
 import social.firefly.core.designsystem.theme.FfTheme
 import social.firefly.core.ui.common.dialog.FfDialog
+import social.firefly.core.ui.common.text.MediumTextTitle
 import social.firefly.core.ui.common.utils.PreviewTheme
 
 @Composable
@@ -36,6 +42,10 @@ fun ChooseAccountDialog(
 
     if (isOpen) {
         FfDialog(
+            properties = DialogProperties(
+                dismissOnClickOutside = false,
+                dismissOnBackPress = false,
+            ),
             onDismissRequest = { viewModel.onDismissRequest() },
         ) {
             DialogContent(
@@ -51,11 +61,13 @@ private fun DialogContent(
     accounts: List<ChooseAccountUiState>,
     chooseAccountInteractions: ChooseAccountInteractions,
 ) {
-    Column {
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        MediumTextTitle(text = stringResource(id = R.string.choose_account_dialog_title))
+        Spacer(modifier = Modifier.height(8.dp))
         accounts.forEach { account ->
             Account(
-                modifier = Modifier.
-                    padding(4.dp),
                 uiState = account,
                 chooseAccountInteractions = chooseAccountInteractions,
             )
@@ -76,9 +88,12 @@ private fun Account(
 
     Row(
         modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(8.dp))
             .clickable {
                 chooseAccountInteractions.onAccountClicked(accountId, domain)
-            },
+            }
+            .padding(8.dp),
         verticalAlignment = Alignment.CenterVertically,
     ) {
         Avatar(avatarUrl)
--- core/ui/common/src/main/kotlin/social/firefly/core/ui/common/dialog/FfDialog.kt
+++ core/ui/common/src/main/kotlin/social/firefly/core/ui/common/dialog/FfDialog.kt
@@ -1,28 +1,26 @@
 package social.firefly.core.ui.common.dialog
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
-import social.firefly.core.designsystem.theme.FfTheme
 import social.firefly.core.ui.common.FfCard
 
 @Composable
 fun FfDialog(
     onDismissRequest: () -> Unit,
+    modifier: Modifier = Modifier,
     properties: DialogProperties = DialogProperties(),
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     Dialog(
         onDismissRequest = onDismissRequest,
         properties = properties,
     ) {
         FfCard(
-            modifier = Modifier
-                .background(FfTheme.colors.layer1),
+            modifier = modifier,
             shape = RoundedCornerShape(16.dp)
         ) {
             content()
