3c6900887693f75093cdc7361baa0cd22acc9cc6
John Oberhauser <j.git-global@obez.io>
2024-05-16 16:38:52 -0500

    hooking up use cases

--- feature/settings/src/main/kotlin/social/firefly/feature/settings/contentpreferences/blockedDomains/BlockedDomainsViewModel.kt
+++ feature/settings/src/main/kotlin/social/firefly/feature/settings/contentpreferences/blockedDomains/BlockedDomainsViewModel.kt
@@ -7,11 +7,17 @@ import androidx.paging.PagingConfig
 import androidx.paging.cachedIn
 import androidx.paging.map
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import social.firefly.core.repository.mastodon.DomainBlocksRepository
 import social.firefly.core.repository.paging.sources.DomainBlocksPagingSource
+import social.firefly.core.usecase.mastodon.account.BlockDomain
+import social.firefly.core.usecase.mastodon.account.UnblockDomain
+import timber.log.Timber
 
 class BlockedDomainsViewModel(
     private val domainBlocksRepository: DomainBlocksRepository,
+    private val blockDomain: BlockDomain,
+    private val unblockDomain: UnblockDomain,
 ) : ViewModel(), BlockedDomainInteractions {
 
     val blockedDomains = Pager(
@@ -30,10 +36,22 @@ class BlockedDomainsViewModel(
     }.cachedIn(viewModelScope)
 
     override fun onUnblockClicked(domain: String) {
-
+        viewModelScope.launch {
+            try {
+                unblockDomain(domain)
+            } catch (e: UnblockDomain.UnblockDomainFailedException) {
+                Timber.e(e)
+            }
+        }
     }
 
     override fun onBlockClicked(domain: String) {
-
+        viewModelScope.launch {
+            try {
+                blockDomain(domain)
+            } catch (e: BlockDomain.BlockDomainFailedException) {
+                Timber.e(e)
+            }
+        }
     }
 }
