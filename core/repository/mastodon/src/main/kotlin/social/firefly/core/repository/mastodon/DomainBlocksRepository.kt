83a4de10c796463afe2db9d12f6a677db83324db
John Oberhauser <j.git-global@obez.io>
2024-05-16 09:47:22 -0500

    adding repository

---
+++ core/repository/mastodon/src/main/kotlin/social/firefly/core/repository/mastodon/DomainBlocksRepository.kt
@@ -0,0 +1,8 @@
+package social.firefly.core.repository.mastodon
+
+import social.firefly.core.network.mastodon.DomainBlocksApi
+
+class DomainBlocksRepository(
+    private val api: DomainBlocksApi,
+) {
+}
