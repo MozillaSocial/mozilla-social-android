5dd22afeb4668467137a8b21eb3adb6108b12518
John Oberhauser <j.git-global@obez.io>
2024-05-24 12:04:17 -0500

    generating baseline profile

--- baselineprofile/build.gradle.kts
+++ baselineprofile/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.api.dsl.ManagedVirtualDevice
+
 plugins {
     id("social.firefly.android.baselineProfile")
 }
@@ -5,12 +7,24 @@ plugins {
 android {
     namespace = "org.mozilla.social.baselineprofile"
     targetProjectPath = ":app"
+
+    // This code creates the gradle managed device used to generate baseline profiles.
+    // To use GMD please invoke generation through the command line:
+    // ./gradlew :app:generateBaselineProfile
+    testOptions.managedDevices.devices {
+        create<ManagedVirtualDevice>("pixel8Api34") {
+            device = "Pixel 8"
+            apiLevel = 34
+            systemImageSource = "google"
+        }
+    }
 }
 
 // This is the configuration block for the Baseline Profile plugin.
 // You can specify to run the generators on a managed devices or connected devices.
 baselineProfile {
-    useConnectedDevices = true
+    managedDevices += "pixel8Api34"
+    useConnectedDevices = false
 }
 
 dependencies {
@@ -20,12 +34,12 @@ dependencies {
     implementation(libs.androidx.benchmark.macro.junit4)
 }
 
-//androidComponents {
-//    onVariants { v ->
-//        val artifactsLoader = v.artifacts.getBuiltArtifactsLoader()
-//        v.instrumentationRunnerArguments.put(
-//            "targetAppId",
-//            v.testedApks.map { artifactsLoader.load(it)?.applicationId }
-//        )
-//    }
-//}
+androidComponents {
+    onVariants { v ->
+        val artifactsLoader = v.artifacts.getBuiltArtifactsLoader()
+        v.instrumentationRunnerArguments.put(
+            "targetAppId",
+            v.testedApks.map { artifactsLoader.load(it)?.applicationId }
+        )
+    }
+}
