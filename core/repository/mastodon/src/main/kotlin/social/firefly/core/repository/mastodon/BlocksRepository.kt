be10860c5db9007638e7d5d8e3da2783c05e07dd
JohnOberhauser <14130581+JohnOberhauser@users.noreply.github.com>
2024-05-24 19:39:49 -0500

    Converting blocked users to ktor (#547)
    Co-authored-by: John Oberhauser <j.git-global@obez.io>

--- core/repository/mastodon/src/main/kotlin/social/firefly/core/repository/mastodon/BlocksRepository.kt
+++ core/repository/mastodon/src/main/kotlin/social/firefly/core/repository/mastodon/BlocksRepository.kt
@@ -15,12 +15,16 @@ import social.firefly.common.toHeaderLink
 import social.firefly.core.database.dao.BlocksDao
 import social.firefly.core.database.model.entities.accountCollections.BlockWrapper
 import social.firefly.core.database.model.entities.accountCollections.DatabaseBlock
+import social.firefly.core.model.Account
 import social.firefly.core.model.BlockedUser
 import social.firefly.core.model.paging.AccountPagingWrapper
+import social.firefly.core.model.paging.MastodonPagedResponse
 import social.firefly.core.network.mastodon.BlocksApi
+import social.firefly.core.network.mastodon.utils.toMastodonPagedResponse
 import social.firefly.core.repository.mastodon.model.block.toAccountsList
 import social.firefly.core.repository.mastodon.model.block.toBlockedUser
 import social.firefly.core.repository.mastodon.model.block.toPagingLinks
+import social.firefly.core.repository.mastodon.model.status.toExternalModel
 
 class BlocksRepository(private val api: BlocksApi, private val dao: BlocksDao) {
 
@@ -43,32 +47,17 @@ class BlocksRepository(private val api: BlocksApi, private val dao: BlocksDao) {
         }
     }
 
-
     suspend fun getBlocks(
         maxId: String? = null,
         sinceId: String? = null,
         minId: String? = null,
         limit: Int? = null,
-    ): AccountPagingWrapper {
-        val response = api.getBlocks(
-            maxId = maxId,
-            sinceId = sinceId,
-            minId = minId,
-            limit = limit
-        )
-
-        if (!response.isSuccessful) {
-            throw HttpException(response)
-        }
-
-        val pagingLinks = response.toPagingLinks()
-
-        return AccountPagingWrapper(
-            accounts = response.toAccountsList(),
-            nextPage = pagingLinks?.getNext()?.toHeaderLink(),
-            prevPage = pagingLinks?.getPrev()?.toHeaderLink(),
-        )
-    }
+    ): MastodonPagedResponse<Account> = api.getBlocks(
+        maxId = maxId,
+        sinceId = sinceId,
+        minId = minId,
+        limit = limit
+    ).toMastodonPagedResponse { it.toExternalModel() }
 
     suspend fun insertAll(databaseAccounts: List<DatabaseBlock>) = dao.upsertAll(databaseAccounts)
 
