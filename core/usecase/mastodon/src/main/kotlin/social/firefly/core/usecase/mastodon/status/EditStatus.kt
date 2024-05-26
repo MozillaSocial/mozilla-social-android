7bbc3ab6e5881528b86115fba8938ce7e741b563
John Oberhauser <j.git-global@obez.io>
2024-05-26 11:11:21 -0500

    detekt fix

--- core/usecase/mastodon/src/main/kotlin/social/firefly/core/usecase/mastodon/status/EditStatus.kt
+++ core/usecase/mastodon/src/main/kotlin/social/firefly/core/usecase/mastodon/status/EditStatus.kt
@@ -7,20 +7,17 @@ import kotlinx.coroutines.async
 import social.firefly.common.annotations.PreferUseCase
 import social.firefly.common.utils.StringFactory
 import social.firefly.core.model.ImageState
-import social.firefly.core.model.MediaUpdate
 import social.firefly.core.model.StatusVisibility
 import social.firefly.core.model.request.MediaAttributes
 import social.firefly.core.model.request.PollCreate
 import social.firefly.core.model.request.StatusCreate
 import social.firefly.core.navigation.usecases.ShowSnackbar
-import social.firefly.core.repository.mastodon.MediaRepository
 import social.firefly.core.repository.mastodon.StatusRepository
 import social.firefly.core.repository.mastodon.TimelineRepository
 import social.firefly.core.usecase.mastodon.R
 
 class EditStatus(
     private val externalScope: CoroutineScope,
-    private val mediaApi: MediaRepository,
     private val statusRepository: StatusRepository,
     private val saveStatusToDatabase: SaveStatusToDatabase,
     private val timelineRepository: TimelineRepository,
