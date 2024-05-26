9dc79878aebd157e64a38a4e25cfd3f9a63d26e0
John Oberhauser <j.git-global@obez.io>
2024-05-26 15:45:03 -0500

    updating trends api to use ktor

--- core/network/mastodon/src/main/kotlin/social/firefly/core/network/mastodon/TrendsApi.kt
+++ core/network/mastodon/src/main/kotlin/social/firefly/core/network/mastodon/TrendsApi.kt
@@ -1,28 +1,23 @@
 package social.firefly.core.network.mastodon
 
-import retrofit2.http.GET
-import retrofit2.http.Query
 import social.firefly.core.network.mastodon.model.responseBody.NetworkHashTag
 import social.firefly.core.network.mastodon.model.responseBody.NetworkLink
 import social.firefly.core.network.mastodon.model.responseBody.NetworkStatus
 
 interface TrendsApi {
 
-    @GET("/api/v1/trends/tags")
     suspend fun getTrendingTags(
-        @Query("limit") limit: Int? = null,
-        @Query("offset") offset: Int? = null,
+        limit: Int? = null,
+        offset: Int? = null,
     ): List<NetworkHashTag>
 
-    @GET("/api/v1/trends/statuses")
     suspend fun getTrendingStatuses(
-        @Query("limit") limit: Int? = null,
-        @Query("offset") offset: Int? = null,
+        limit: Int? = null,
+        offset: Int? = null,
     ): List<NetworkStatus>
 
-    @GET("/api/v1/trends/links")
     suspend fun getTrendingLinks(
-        @Query("limit") limit: Int? = null,
-        @Query("offset") offset: Int? = null,
+        limit: Int? = null,
+        offset: Int? = null,
     ): List<NetworkLink>
 }
