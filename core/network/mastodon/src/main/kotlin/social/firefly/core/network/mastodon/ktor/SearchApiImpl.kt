55f6d32bb9f5730d4ea9ab69c86e8dda195af8b6
John Oberhauser <j.git-global@obez.io>
2024-05-26 14:08:47 -0500

    cleanup

--- core/network/mastodon/src/main/kotlin/social/firefly/core/network/mastodon/ktor/SearchApiImpl.kt
+++ core/network/mastodon/src/main/kotlin/social/firefly/core/network/mastodon/ktor/SearchApiImpl.kt
@@ -4,7 +4,6 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.http.URLProtocol
-import io.ktor.http.parameters
 import io.ktor.http.path
 import social.firefly.core.network.mastodon.SearchApi
 import social.firefly.core.network.mastodon.model.responseBody.NetworkSearchResult
