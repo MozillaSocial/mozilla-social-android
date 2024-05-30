28c9e9f5d8c7a548b4d16217184b37da286bab84
John Oberhauser <j.git-global@obez.io>
2024-05-30 10:54:25 -0500

    sentry only when allow analytics

--- app/src/main/kotlin/social/firefly/MainApplication.kt
+++ app/src/main/kotlin/social/firefly/MainApplication.kt
@@ -1,16 +1,18 @@
 package social.firefly
 
 import android.app.Application
-import android.os.Build
 import coil.ImageLoader
 import coil.ImageLoaderFactory
-import coil.decode.GifDecoder
 import coil.decode.ImageDecoderDecoder
 import coil.decode.VideoFrameDecoder
 import coil.memory.MemoryCache
 import io.sentry.SentryLevel
 import io.sentry.android.core.SentryAndroid
 import io.sentry.android.timber.SentryTimberIntegration
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import org.koin.android.ext.android.get
 import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
@@ -34,9 +36,8 @@ import social.firefly.feature.report.reportModule
 import social.firefly.feature.settings.settingsModule
 import social.firefly.feature.thread.threadModule
 import social.firefly.feature.feed.feedModule
-import social.firefly.feature.post.newPostModule
 import social.firefly.feature.search.searchModule
-import social.firefly.BuildConfig
+import social.firefly.core.datastore.AppPreferencesDatastore
 import social.firefly.core.push.pushModule
 import social.firefly.core.ui.chooseAccount.chooseAccountModule
 import social.firefly.feature.bookmarks.bookmarksModule
@@ -47,38 +48,45 @@ class MainApplication : Application(), ImageLoaderFactory {
 
     private lateinit var authCredentialObserver: AuthCredentialObserver
     private val analytics: AppAnalytics by inject()
+    private val appPreferencesDatastore: AppPreferencesDatastore by inject()
 
     override fun onCreate() {
         super.onCreate()
         initializeAppVersion()
-        initializeTimberAndSentry()
         initializeKoin()
+        initializeTimberAndSentry()
         initializeAnalytics()
         initializeAuthCredentialInterceptor()
     }
 
     private fun initializeTimberAndSentry() {
-        SentryAndroid.init(this) { options ->
-            options.apply {
-                setDiagnosticLevel(SentryLevel.ERROR)
-                dsn = BuildConfig.sentryDsn
-                isDebug = BuildConfig.DEBUG
-                environment = BuildConfig.BUILD_TYPE
-                isEnableUserInteractionTracing = true
-                isAttachScreenshot = false
-                isAttachViewHierarchy = true
-                sampleRate = 1.0
-                profilesSampleRate = 1.0
-                if (!BuildConfig.DEBUG) {
-                    addIntegration(
-                        SentryTimberIntegration(
-                            minEventLevel = SentryLevel.ERROR,
-                            minBreadcrumbLevel = SentryLevel.INFO
-                        )
-                    )
-                } else {
-                    Timber.plant(Timber.DebugTree())
+        CoroutineScope(Dispatchers.Default).launch {
+            if (appPreferencesDatastore.allowAnalytics.first()) {
+                SentryAndroid.init(this@MainApplication) { options ->
+                    options.apply {
+                        setDiagnosticLevel(SentryLevel.ERROR)
+                        dsn = BuildConfig.sentryDsn
+                        isDebug = BuildConfig.DEBUG
+                        environment = BuildConfig.BUILD_TYPE
+                        isEnableUserInteractionTracing = true
+                        isAttachScreenshot = false
+                        isAttachViewHierarchy = false
+                        sampleRate = 1.0
+                        profilesSampleRate = 1.0
+                        if (!BuildConfig.DEBUG) {
+                            addIntegration(
+                                SentryTimberIntegration(
+                                    minEventLevel = SentryLevel.ERROR,
+                                    minBreadcrumbLevel = SentryLevel.INFO
+                                )
+                            )
+                        } else {
+                            Timber.plant(Timber.DebugTree())
+                        }
+                    }
                 }
+            } else if (BuildConfig.DEBUG) {
+                Timber.plant(Timber.DebugTree())
             }
         }
     }
@@ -112,11 +120,7 @@ class MainApplication : Application(), ImageLoaderFactory {
     override fun newImageLoader(): ImageLoader =
         ImageLoader.Builder(this)
             .components {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                    add(ImageDecoderDecoder.Factory())
-                } else {
-                    add(GifDecoder.Factory())
-                }
+                add(ImageDecoderDecoder.Factory())
                 add(VideoFrameDecoder.Factory())
             }
             .memoryCache {
