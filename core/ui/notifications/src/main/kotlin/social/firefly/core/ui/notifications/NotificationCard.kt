ebd7bb928abd0e61613cb2f61fc0698dba30a7a8
John Oberhauser <j.git-global@obez.io>
2024-05-31 14:30:51 -0500

    fixing notification meta data height

--- core/ui/notifications/src/main/kotlin/social/firefly/core/ui/notifications/NotificationCard.kt
+++ core/ui/notifications/src/main/kotlin/social/firefly/core/ui/notifications/NotificationCard.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -181,7 +182,7 @@ private fun NotificationCard(
             Column {
                 NotificationMetaData(
                     modifier = Modifier
-                        .height(48.dp),
+                        .heightIn(min = 48.dp),
                     uiState = uiState,
                 )
                 content()
