919012b12039d6b4b58c8d93ccf23b5500a0b359
John Oberhauser <j.git-global@obez.io>
2024-05-31 11:15:10 -0500

    fixing scroll on report screen

--- feature/report/src/main/kotlin/social/firefly/feature/report/step3/ReportScreen3.kt
+++ feature/report/src/main/kotlin/social/firefly/feature/report/step3/ReportScreen3.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -71,8 +73,7 @@ private fun ReportScreen3(
 ) {
     FfSurface {
         Column(
-            modifier =
-            Modifier
+            modifier = Modifier
                 .fillMaxHeight()
                 .systemBarsPadding(),
         ) {
@@ -154,8 +155,8 @@ private fun MiddleContent(
     reportInteractions: ReportScreen3Interactions,
 ) {
     Column(
-        modifier =
-        modifier
+        modifier = modifier
+            .verticalScroll(rememberScrollState())
             .padding(16.dp),
     ) {
         Text(
