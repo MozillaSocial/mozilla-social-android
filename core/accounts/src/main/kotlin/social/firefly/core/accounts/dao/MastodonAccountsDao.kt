3eec8c6bccaefccb160331dbc6b79c88a09d3799
John Oberhauser <j.git-global@obez.io>
2024-06-14 16:26:56 -0400

    queries

--- core/accounts/src/main/kotlin/social/firefly/core/accounts/dao/MastodonAccountsDao.kt
+++ core/accounts/src/main/kotlin/social/firefly/core/accounts/dao/MastodonAccountsDao.kt
@@ -1,8 +1,41 @@
 package social.firefly.core.accounts.dao
 
 import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Transaction
+import kotlinx.coroutines.flow.Flow
 import social.firefly.core.accounts.model.MastodonAccount
 
 @Dao
 interface MastodonAccountsDao : BaseDao<MastodonAccount> {
+
+    @Transaction
+    @Query(
+        "SELECT * from mastodonAccounts " +
+        "WHERE accountId IN " +
+        "(" +
+            "SELECT accountId FROM activeAccount" +
+        ") " +
+        "AND " +
+        "domain IN " +
+        "(" +
+            "SELECT domain FROM activeAccount" +
+        ") "
+    )
+    suspend fun getActiveAccount(): MastodonAccount
+
+    @Transaction
+    @Query(
+        "SELECT * from mastodonAccounts " +
+        "WHERE accountId IN " +
+        "(" +
+            "SELECT accountId FROM activeAccount" +
+        ") " +
+        "AND " +
+        "domain IN " +
+        "(" +
+            "SELECT domain FROM activeAccount" +
+        ") "
+    )
+    suspend fun getActiveAccountFlow(): Flow<MastodonAccount>
 }
