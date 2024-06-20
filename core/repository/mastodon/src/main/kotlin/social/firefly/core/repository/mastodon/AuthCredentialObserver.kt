cdf53ecfe3c6d03644ff1b3e2fb88a9394764cbd
John Oberhauser <j.git-global@obez.io>
2024-06-20 13:16:21 -0400

    saving

--- core/repository/mastodon/src/main/kotlin/social/firefly/core/repository/mastodon/AuthCredentialObserver.kt
+++ core/repository/mastodon/src/main/kotlin/social/firefly/core/repository/mastodon/AuthCredentialObserver.kt
@@ -1,43 +1,36 @@
 package social.firefly.core.repository.mastodon
 
 import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.launch
-import social.firefly.core.datastore.UserPreferencesDatastoreManager
+import social.firefly.core.accounts.AccountsManager
 import social.firefly.core.network.mastodon.interceptors.AuthCredentialInterceptor
 
 /**
  * Keeps the domain and access token in [AuthCredentialInterceptor] up to date
  */
-@OptIn(DelicateCoroutinesApi::class, ExperimentalCoroutinesApi::class)
+@OptIn(DelicateCoroutinesApi::class)
 class AuthCredentialObserver(
-    userPreferencesDatastoreManager: UserPreferencesDatastoreManager,
     authCredentialInterceptor: AuthCredentialInterceptor,
+    accountsManager: AccountsManager,
 ) {
     init {
         GlobalScope.launch {
             coroutineScope {
-                userPreferencesDatastoreManager.activeUserDatastore.flatMapLatest {
-                    it.accessToken
-                }.filterNotNull().collectLatest {
-                    if (it.isNotBlank()) {
-                        authCredentialInterceptor.accessToken = it
+                accountsManager.getActiveAccountFlow().collectLatest {
+                    if (it.accessToken.isNotBlank()) {
+                        authCredentialInterceptor.accessToken = it.accessToken
                     }
                 }
             }
         }
 
         GlobalScope.launch {
-            userPreferencesDatastoreManager.activeUserDatastore.flatMapLatest {
-                it.domain
-            }.filterNotNull().collectLatest {
-                if (it.isNotBlank()) {
-                    authCredentialInterceptor.domain = it
+            accountsManager.getActiveAccountFlow().collectLatest {
+                if (it.domain.isNotBlank()) {
+                    authCredentialInterceptor.domain = it.domain
                 }
             }
         }
