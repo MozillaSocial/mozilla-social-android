ea8f7721dde6eb819bff20cdc127b9ded68acf1b
John Oberhauser <j.git-global@obez.io>
2024-06-20 13:16:11 -0400

    saving

--- core/repository/mastodon/build.gradle.kts
+++ core/repository/mastodon/build.gradle.kts
@@ -10,9 +10,9 @@ android {
 dependencies {
     implementation(project(":core:common"))
     implementation(project(":core:database"))
-    implementation(project(":core:datastore"))
     implementation(project(":core:model"))
     implementation(project(":core:network:mastodon"))
+    implementation(project(":core:accounts"))
 
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.kotlinx.datetime)
